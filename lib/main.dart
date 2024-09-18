b5ae585bf24c93b16ae1f0bee839791febbd6ebc
Channaka Gunawardhane <channakagunawardana@gmail.com>
2024-09-18 20:26:22 +0530

    firebase and bloc implementation initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_expensee/simple_bloc_observer.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
   runApp(const MyApp());
 }
---
+++ lib/screens/add_expense/blocs/create_category_bloc/create_category_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+part 'create_category_event.dart';
+part 'create_category_state.dart';
+
+class CreateCategoryBloc
+    extends Bloc<CreateCategoryEvent, CreateCategoryState> {
+  CreateCategoryBloc() : super(CreateCategoryInitial()) {
+    on<CreateCategoryEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
